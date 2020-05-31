b778776c1e1e0b83ea5fc248efc41ebb141edf7e
Liu Fan <liufcc@gmail.com>
2020-05-31 12:51:05 +0800

    First version for Spark on Windows

---
+++ 04-SparkCore/src/Simple/SparkDemo.scala
@@ -0,0 +1,15 @@
+package Simple
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object SparkDemo {
+   def main(args: Array[String]) {
+    val stocksPath = "C:/dev/github/BEAN2020June/04-SparkCore/data/linecount.txt"
+    val conf = new SparkConf().setAppName("Counting Lines").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile(stocksPath, 2)
+    val totalLines = data.count()
+    println("Total number of Lines: %s".format(totalLines))
+  }
+}
